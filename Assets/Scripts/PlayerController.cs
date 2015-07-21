04571eb2010af3037415552c258966e165b07316
Tamara Tanasescu <gokimster@yahoo.com>
2015-07-21 17:37:20 +0300

    Animations and Controller update + new art

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,24 +12,23 @@ public class PlayerController : MonoBehaviour {
 	//for jumping
 	public bool grounded = false;
 	public float jumpForce;
-	
-	//for Collision Raycasting
-	//behind
-	//public Transform lTBegin,lTEnd;
-	//public Transform lBBegin,lBEnd;
-	//front
-	//public Transform rTBegin,rTEnd;
-	//public Transform rBBegin, rBEnd;
-	//down
-	//public Transform bRBegin, bREnd;
-	//public Transform bLBegin, bLEnd;
-	//public bool isColliding;
+	public bool isRolling;
+
+	//colliders
+	[SerializeField]
+	private BoxCollider2D[] colliders;
+	private int currentColliderIndex;
 	
 	// Use this for initialization
 	void Start () 
 	{
 		speed = 10f;
-		jumpForce = 300f;
+		jumpForce = 600f;
+		anim = GetComponent<Animator> ();
+		SetJumping ();
+		isRolling = false;
+		currentColliderIndex = 0;
+		SetCollider (currentColliderIndex);
 	}
 	
 	// Update is called once per frame
@@ -38,10 +37,13 @@ public class PlayerController : MonoBehaviour {
 		DebugLines();
 		//grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
 		SetGrounded ();
+		SetJumping ();
 		//anim.SetBool ("ground", grounded);
 		//anim.SetFloat ("vSpeed", rigidbody2D.velocity.y);
 		
 		Jump ();
+		Roll ();
+
 		//CheckFrontColission ();
 		//anim.SetFloat ("speed", Mathf.Abs (move));
 		GetComponent<Rigidbody2D>().velocity = new Vector2 (speed, GetComponent<Rigidbody2D>().velocity.y);
@@ -49,7 +51,8 @@ public class PlayerController : MonoBehaviour {
 	}
 	void SetGrounded()
 	{
-		RaycastHit2D hit = Physics2D.Raycast (transform.position + new Vector3(0, -(transform.lossyScale.y/1.8f), 0), Vector2.down, .5f);
+		LayerMask lMask = 1 << LayerMask.NameToLayer ("Ground");
+		RaycastHit2D hit = Physics2D.Raycast (transform.position + new Vector3(0, -(transform.lossyScale.y), 0), Vector2.down, .5f, lMask);
 		if (hit.collider != null && hit.collider.tag.Equals ("Ground"))
 		{
 			grounded = true;
@@ -60,9 +63,22 @@ public class PlayerController : MonoBehaviour {
 		
 	}
 
+	void SetJumping ()
+	{
+		anim.SetBool("isJumping", false);
+		anim.SetBool ("isFalling", false);
+		if (transform.GetComponent<Rigidbody2D> ().velocity.y > 0) {
+			anim.SetBool("isJumping", true);
+		}
+		if (transform.GetComponent<Rigidbody2D> ().velocity.y < 0) {
+			anim.SetBool ("isFalling", true);
+		}
+	}
+
 	void DebugLines()
 	{
 		Debug.DrawRay (transform.position + new Vector3(0, -(transform.lossyScale.y/1.8f), 0), Vector2.down, Color.blue);
+		Debug.Log (transform.GetComponent<Rigidbody2D> ().velocity.y.ToString());
 	}
 	
 	//jump action 
@@ -76,12 +92,22 @@ public class PlayerController : MonoBehaviour {
 		
 	}
 
-	void Crouch ()
+	void Roll ()
 	{
-		if(grounded && Input.GetKeyDown (KeyCode.S))
+		isRolling = false;
+		anim.SetBool ("isRolling", false);
+		if(grounded && Input.GetKey (KeyCode.S))
 		{
-
+			isRolling = true;
+			anim.SetBool("isRolling", true);
 		}
+	}
+
+	public void SetCollider(int number)
+	{
+		colliders [currentColliderIndex].enabled = false;
+		currentColliderIndex = number;
+		colliders [currentColliderIndex].enabled = true;
 	}
 
 	void OnTriggerEnter2D (Collider2D col)
